df06a08089b0d501f7599080dfffe9a00d61f307
MicheleMorelli <michele.morelli@london.ac.uk>
2018-02-23 15:20:32 +0000

    logic done

--- stack.c
+++ stack.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define STACK_SIZE 10
 #define STACK_INIT(X) STACK X = {.T = 0}
 
 typedef struct {
     int T; // the stack pointer
-    int st[STACK_SIZE];
+    int st[STACK_SIZE]; 
 } STACK;
 
 void push(STACK *stack, int value);
 int pop(STACK *stack);
 void draw();
-void get_input();
-void overflow();
-void underflow();
-void stack_is_empty();
+int get_input();
+void execute_command(STACK *stack, int input);
+int overflow();
+int underflow();
+int stack_is_empty(STACK *stack);
 
 int main(){
+    srand(time(NULL)); //pushing random numbers
     STACK_INIT(stack);
-    push(&stack, 67);
-    printf("\nT is now %d.\n", stack.T);
-    pop(&stack);
-    pop(&stack);
+
+    while (1==1){
+        execute_command(&stack, get_input()); 
+    }
 
     return 0;
 }
@@ -32,13 +37,13 @@ void push(STACK *stack, int value){
    else{
     stack->T++;
     stack->st[stack->T] = value;
+    printf("Pushed %d onto the stack.\nT is now %d.\n", value, stack->T);
    }
-   printf("Pushed %d onto the stack.\nT is now %d.\n", value, stack->T);
 }
 
 int pop(STACK *stack){
     int returned_value;
-    if (!stack->T){
+    if (stack_is_empty(stack)){
         underflow();
     }
     else{
@@ -50,10 +55,40 @@ int pop(STACK *stack){
 
 }
 
-void overflow(){
+int overflow(){
     printf("OVERFLOW!!\n\n");
+    return 1;
 }
 
-void underflow(){
+int underflow(){
     printf("UNDERFLOW!!\n\n");
+    return 1;
+}
+
+int stack_is_empty(STACK *stack){
+    return (!stack->T) ? 1 : 0;
+}
+
+int get_input(){
+    int val;
+    printf("1- Push\n2- Pop\n3- Show state\n");
+    scanf("%d", &val);
+    return val;
+}
+
+void execute_command(STACK *stack, int input){
+    switch(input){
+        case 1:
+            push(stack, rand() % 50 + 1); //using random nums for simplicity
+            break;
+
+        case 2:
+            pop(stack);
+            break;
+
+        case 3:
+        default:
+            //draw();
+            break;
+    }
 }
